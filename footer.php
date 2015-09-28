b701ef8b48bfc5104d03f5bdba6545d0c2f0fc7e
marcus.tisater@happiness.se <marcus.tisater@happiness.se>
2015-09-28 22:16:06 +0200

    Added footer widgets

--- footer.php
+++ footer.php
@@ -12,17 +12,36 @@
 ?>
 
 	</div><!-- #content -->
+ </div><!-- #page -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<a href="#" class="scrollToTop icon-chevron-up"> <span> <?php printf('Back To Top') ?> </span> </a> 
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'blogsixteen' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'blogsixteen' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s', 'blogsixteen' ), 'blogsixteen', ''); ?>
-		</div><!-- .site-info -->
-		<?php wp_footer(); ?>
+		<div class="footer-content">
+			<a href="#" class="scrollToTop icon-chevron-up"> <span> <?php printf('Back To Top') ?> </span> </a>
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'blogsixteen' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'blogsixteen' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s', 'blogsixteen' ), 'blogsixteen', ''); ?>
+			</div><!-- .site-info -->
+			<?php wp_footer(); ?>
+		</div>
+		<div id="footer-widgets">
+			<h3> I love WordPress </h3>
+			<div class="footer-widget-container">
+				<div class="footer-widget-left">
+				<?php
+				if(is_active_sidebar('sidebar-footer-1')){
+					dynamic_sidebar('sidebar-footer-1');
+				} ?>
+				</div>
+				<div class="footer-widget-right">
+					<?php
+					if(is_active_sidebar('sidebar-footer-2')){
+						dynamic_sidebar('sidebar-footer-2');
+					}?>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function blogsixteen_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'blogsixteen' ) 
+		'primary' => esc_html__( 'Primary Menu', 'blogsixteen' )
 	) );
 
 	/*
@@ -108,6 +108,25 @@ function blogsixteen_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Sidebar 1', 'blogsixteen' ),
+		'id'            => 'sidebar-footer-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Sidebar 2', 'blogsixteen' ),
+		'id'            => 'sidebar-footer-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
 }
 add_action( 'widgets_init', 'blogsixteen_widgets_init' );
 
