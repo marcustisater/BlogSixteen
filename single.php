90126e2afb320bfebcd0cadb0325f866bd4a20ec
marcus.tisater@happiness.se <marcus.tisater@happiness.se>
2015-10-09 17:52:58 +0200

    Added styling for 1.0.3

--- single.php
+++ single.php
@@ -7,25 +7,7 @@
  * @package BlogSixteen
  */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="single-post hfeed site">
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="back-to">
-			<a href="/"> <button class="icon-home" type="button" name="button"> </button> </a>
-		</div>
-	</header>
+get_header();?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main no-sidebar" role="main">
